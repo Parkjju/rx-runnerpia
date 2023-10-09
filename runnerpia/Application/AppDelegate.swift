dd15ffa63607b2cc9733303b4d6b3d4197d54485
Parkjju <rudwns3927@gmail.com>
2023-10-09 12:24:10 +0900

    [Feat] 탭바 세팅

--- runnerpia/Application/AppDelegate.swift
+++ runnerpia/Application/AppDelegate.swift
@@ -16,9 +16,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         window = UIWindow(frame: UIScreen.main.bounds)
-        window?.makeKeyAndVisible()
-        window?.rootViewController = ViewController()
-        window?.backgroundColor = .systemBackground
+        guard let window = window else { return false }
+        MainViewManager.shared.show(in: window)
         return true
     }
     
