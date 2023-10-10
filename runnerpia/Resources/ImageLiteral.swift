08ef0b1424a7c569fedc3837388a0e0ee930636e
Parkjju <rudwns3927@gmail.com>
2023-10-10 16:42:55 +0900

    [Chore] 애셋 리터럴 연결

---
+++ runnerpia/Resources/ImageLiteral.swift
@@ -0,0 +1,24 @@
+//
+//  ImageLiteral.swift
+//  runnerpia
+//
+//  Created by 박경준 on 2023/10/10.
+//
+
+import UIKit
+
+enum ImageLiteral {
+    static var imgFollowRoute: UIImage {.load(named: "imgFollowRoute")}
+    static var imgBookmarkFilled: UIImage {.load(named: "imgBookmarkFilled")}
+    static var imgBookmarkLine: UIImage {.load(named: "imgBookmarkLine")}
+    static var imgCalendarLine: UIImage {.load(named: "imgCalendarLine")}
+    static var imgDeleteLine: UIImage {.load(named: "imgDeleteLine" )}
+    static var imgImportantFilled: UIImage {.load(named: "imgImportantFilled")}
+    static var imgLocationPinLine: UIImage {.load(named: "imgLocationPinLine")}
+    static var imgLogoutLine: UIImage {.load(named: "imgLogoutLine")}
+    static var imgMyPage: UIImage {.load(named: "imgMyPage")}
+    static var imgReviewLine: UIImage {.load(named: "imgReviewLine")}
+    static var imgRouteLine: UIImage {.load(named: "imgRouteLine")}
+    static var imgTimeLine: UIImage {.load(named: "imgTimeLine")}
+    static var imgHome: UIImage {.load(named: "imgHome")}
+}
