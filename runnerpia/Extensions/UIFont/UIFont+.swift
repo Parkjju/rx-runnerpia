bc06e1bba7436f71d76e027b8a386bcef687356a
Parkjju <rudwns3927@gmail.com>
2023-10-10 17:11:32 +0900

    [Chore] 폰트 익스텐션 연결

--- runnerpia/Extensions/UIFont/UIFont+.swift
+++ runnerpia/Extensions/UIFont/UIFont+.swift
@@ -5,4 +5,22 @@
 //  Created by 박경준 on 2023/10/10.
 //
 
-import Foundation
+import UIKit
+
+enum PretendardStyle: String {
+    case black = "Pretendard-Black"
+    case bold = "Pretendard-Bold"
+    case extraBold = "Pretendard-ExtraBold"
+    case extraLight = "Pretendard-ExtraLight"
+    case light = "Pretendard-Light"
+    case medium = "Pretendard-Medium"
+    case regular = "Pretendard-Regular"
+    case semibold = "Pretendard-SemiBold"
+    case thin = "Pretendard-Thin"
+}
+
+extension UIFont {
+    static func pretendard(_ style: PretendardStyle, ofSize size: CGFloat) -> UIFont {
+        return UIFont(name: style.rawValue, size: size)!
+    }
+}
