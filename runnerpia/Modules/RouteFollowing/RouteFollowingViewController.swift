363ea1cbb8fea93e42853c6f0de507652509e4a0
Parkjju <rudwns3927@gmail.com>
2023-10-10 22:56:25 +0900

    [Feat] 네비게이션 바 타이틀 변경

--- runnerpia/Modules/RouteFollowing/RouteFollowingViewController.swift
+++ runnerpia/Modules/RouteFollowing/RouteFollowingViewController.swift
@@ -15,7 +15,7 @@ class RouteFollowingViewController: BaseViewController {
     // MARK: - Functions
     
     override func configUI() {
-        self.title = "추천 경로"
+        self.navigationItem.title = "추천 경로"
         view.backgroundColor = .grey200
     }
 }
