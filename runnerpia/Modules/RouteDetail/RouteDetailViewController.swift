30ca4d878f8621008b8e93b53535c11493d21089
Parkjju <rudwns3927@gmail.com>
2023-10-10 15:48:41 +0900

    [Feat] 기본 뷰 세팅

---
+++ runnerpia/Modules/RouteDetail/RouteDetailViewController.swift
@@ -0,0 +1,12 @@
+//
+//  RouteDetailViewController.swift
+//  runnerpia
+//
+//  Created by 박경준 on 2023/10/10.
+//
+
+import UIKit
+
+class RouteDetailViewController: BaseViewController {
+
+}
---
+++ runnerpia/Modules/RouteFollowing/RouteFollowingViewController.swift
@@ -0,0 +1,12 @@
+//
+//  RouteFollowingViewController.swift
+//  runnerpia
+//
+//  Created by 박경준 on 2023/10/10.
+//
+
+import UIKit
+
+class RouteFollowingViewController: BaseViewController {
+    
+}
