0b6f73c917102b1ed820c4f03dbafa667e5d9335
Parkjju <rudwns3927@gmail.com>
2023-10-10 22:49:45 +0900

    [Feat] 경로 따라가기 클래스 정의

---
+++ runnerpia/Modules/RouteRunning/RouteRunningViewController.swift
@@ -0,0 +1,27 @@
+//
+//  RouteRunningViewController.swift
+//  runnerpia
+//
+//  Created by Jun on 2023/10/10.
+//
+
+import UIKit
+
+class RouteRunningViewController: BaseViewController {
+    
+    // MARK: - Subviews
+    
+    // MARK: - Properties
+    
+    // MARK: - Life Cycles
+    
+    // MARK: - Functions
+    
+    override func render() {
+        
+    }
+    
+    override func configUI() {
+        view.backgroundColor = .white
+    }
+}
