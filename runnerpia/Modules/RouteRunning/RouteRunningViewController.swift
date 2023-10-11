83e74f339a84d9cbb5d6378ac526fd1414ad07d7
Parkjju <rudwns3927@gmail.com>
2023-10-11 15:38:22 +0900

    [Feat] 경로 기록 뷰 인풋 아웃풋 정의

--- runnerpia/Modules/RouteRunning/RouteRunningViewController.swift
+++ runnerpia/Modules/RouteRunning/RouteRunningViewController.swift
@@ -8,6 +8,7 @@
 import UIKit
 import CoreLocation
 import NMapsMap
+import RxSwift
 
 class RouteRunningViewController: BaseViewController {
     
@@ -16,6 +17,8 @@ class RouteRunningViewController: BaseViewController {
     
     // MARK: - Properties
     var viewModel: RouteRunningViewModel
+    let marker = NMFMarker()
+    let viewDidLoadTrigger = PublishSubject<Void>()
     
     // MARK: - Life Cycles
     init(viewModel: RouteRunningViewModel) {
@@ -25,6 +28,11 @@ class RouteRunningViewController: BaseViewController {
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        viewDidLoadTrigger.onNext(())
     }
     
     // MARK: - Functions
@@ -40,5 +48,32 @@ class RouteRunningViewController: BaseViewController {
     override func configUI() {
         view.backgroundColor = .white
         navigationController?.isNavigationBarHidden = false
+        
+        marker.position = NMGLatLng(lat: 0, lng: 0)
+        marker.mapView = mapView
+        marker.iconImage = NMFOverlayImage(image: ImageLiteral.imgCustomMarker)
+    }
+    
+    override func bindViewModel() {
+        let input = RouteRunningViewModel.Input(viewDidLoadTrigger: viewDidLoadTrigger, didChangeCoordinate: LocationManager.shared.rx.didUpdateLocations)
+        let output = viewModel.transform(input: input)
+        
+        output.startPosition
+            .subscribe(onNext: { [unowned self] in
+                guard let coord = $0 else { return }
+                let cameraUpdate = NMFCameraUpdate(scrollTo: NMGLatLng(lat: coord.lat, lng: coord.lng))
+                self.mapView.moveCamera(cameraUpdate)
+                self.marker.position = coord
+            })
+            .disposed(by: disposeBag)
+        
+        output.currentPosition
+            .subscribe(onNext: { [unowned self] in
+                guard let coord = $0 else { return }
+                let cameraUpdate = NMFCameraUpdate(scrollTo: NMGLatLng(lat: coord.lat, lng: coord.lng))
+                self.mapView.moveCamera(cameraUpdate)
+                self.marker.position = coord
+            })
+            .disposed(by: disposeBag)
     }
 }
--- runnerpia/Modules/RouteRunning/RouteRunningViewModel.swift
+++ runnerpia/Modules/RouteRunning/RouteRunningViewModel.swift
@@ -14,23 +14,34 @@ class RouteRunningViewModel: ViewModelType {
     var bag = DisposeBag()
     
     struct Input {
-        let didChangeCoordinate: Observable<Coordinate>
+        let viewDidLoadTrigger: Observable<Void>
+        let didChangeCoordinate: Observable<[CLLocation]>
     }
     
     struct Output {
+        let startPosition: BehaviorRelay<NMGLatLng?>
         let currentPosition: BehaviorRelay<NMGLatLng?>
     }
     
     func transform(input: Input) -> Output {
+        let startPosition = BehaviorRelay<NMGLatLng?>(value: nil)
         let currentPosition = BehaviorRelay<NMGLatLng?>(value: nil)
+        
+        input.viewDidLoadTrigger
+            .subscribe(onNext: {
+                guard let coord = LocationManager.shared.location?.coordinate else { return }
+                let nmg = NMGLatLng(lat: coord.latitude, lng: coord.longitude)
+                startPosition.accept(nmg)
+            })
+            .disposed(by: bag)
         
         input.didChangeCoordinate
             .subscribe(onNext: {
-                let nmg = NMGLatLng(lat: $0.latitude, lng: $0.longitude)
+                let nmg = NMGLatLng(lat: $0.last?.coordinate.latitude ?? 0, lng: $0.last?.coordinate.longitude ?? 0)
                 currentPosition.accept(nmg)
             })
             .disposed(by: bag)
         
-        return Output(currentPosition: currentPosition)
+        return Output(startPosition: startPosition, currentPosition: currentPosition)
     }
 }
