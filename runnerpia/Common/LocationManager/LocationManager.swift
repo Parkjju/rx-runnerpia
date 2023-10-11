14739b652a21cd5184c25c3d58b34fac23aa7678
Parkjju <rudwns3927@gmail.com>
2023-10-11 15:34:38 +0900

    [Feat] LocationManager 싱글톤 클래스 생성

---
+++ runnerpia/Common/LocationManager/LocationManager.swift
@@ -0,0 +1,13 @@
+//
+//  LocationManager.swift
+//  runnerpia
+//
+//  Created by 박경준 on 2023/10/11.
+//
+
+import Foundation
+import CoreLocation
+
+class LocationManager {
+    static let shared = CLLocationManager()
+}
