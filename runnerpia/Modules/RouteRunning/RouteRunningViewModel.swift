3deb63cd3e4bb7c6f9e45bf64562cd1214175be9
Parkjju <rudwns3927@gmail.com>
2023-10-13 00:45:24 +0900

    [Feat] 뷰모델 데이터 정제 로직 구현

--- runnerpia/Modules/RouteRunning/RouteRunningViewModel.swift
+++ runnerpia/Modules/RouteRunning/RouteRunningViewModel.swift
@@ -16,22 +16,36 @@ class RouteRunningViewModel: ViewModelType {
     struct Input {
         let viewDidLoadTrigger: Observable<Void>
         let didChangeCoordinate: Observable<[CLLocation]>
+        let buttonTouchDownEvent: Observable<Void>
+        let buttonTouchUpEvent: Observable<Void>
+        let pauseButtonTapTrigger: Observable<Void>
+        let playButtonTapTrigger: Observable<Void>
     }
     
     struct Output {
         let startPosition: BehaviorRelay<NMGLatLng?>
         let currentPosition: BehaviorRelay<NMGLatLng?>
+        let isRecordStarted: Driver<Bool>
+        let isRecordPaused: Driver<Bool?>
+        let runningTime: Driver<Int>
+        let runningDistance: Driver<Int>
     }
     
     func transform(input: Input) -> Output {
         let startPosition = BehaviorRelay<NMGLatLng?>(value: nil)
         let currentPosition = BehaviorRelay<NMGLatLng?>(value: nil)
+        let recordStartTrigger = BehaviorRelay<Bool>(value: false)
+        let isRecordPaused = BehaviorRelay<Bool?>(value: nil)
+        let runningTime = BehaviorRelay<Int>(value: 0)
+        let lastCoordinate = BehaviorRelay<CLLocation?>(value: nil)
+        let distance = BehaviorRelay<Int>(value: 0)
         
         input.viewDidLoadTrigger
             .subscribe(onNext: {
                 guard let coord = LocationManager.shared.location?.coordinate else { return }
                 let nmg = NMGLatLng(lat: coord.latitude, lng: coord.longitude)
                 startPosition.accept(nmg)
+                lastCoordinate.accept(CLLocation(latitude: coord.latitude, longitude: coord.longitude))
             })
             .disposed(by: bag)
         
@@ -42,6 +56,65 @@ class RouteRunningViewModel: ViewModelType {
             })
             .disposed(by: bag)
         
-        return Output(startPosition: startPosition, currentPosition: currentPosition)
+        input.buttonTouchDownEvent
+            .flatMapLatest {
+                Observable<Int>.interval(.seconds(1), scheduler: MainScheduler.instance).take(until: input.buttonTouchUpEvent)
+            }
+            .subscribe(onNext: { second in
+                if second >= 1 {
+                    recordStartTrigger.accept(true)
+                    isRecordPaused.accept(false)
+                } else {
+                    recordStartTrigger.accept(false)
+                }
+                
+            })
+            .disposed(by: bag)
+        
+        input.pauseButtonTapTrigger
+            .subscribe(onNext: {
+                isRecordPaused.accept(true)
+            })
+            .disposed(by: bag)
+        
+        input.playButtonTapTrigger
+            .subscribe(onNext: {
+                isRecordPaused.accept(false)
+            })
+            .disposed(by: bag)
+        
+        isRecordPaused.asObservable()
+            .flatMapLatest { isPaused -> Observable<Int> in
+                guard let isPaused = isPaused else { return Observable<Int>.empty() }
+                return !isPaused ? Observable<Int>.interval(.seconds(1), scheduler: MainScheduler.instance) : Observable<Int>.empty()
+            }
+            .subscribe(onNext: { _ in
+                runningTime.accept(runningTime.value + 1)
+            })
+            .disposed(by: bag)
+        
+        isRecordPaused.asObservable()
+            .flatMapLatest { isPaused -> Observable<[CLLocation]> in
+                guard let isPaused = isPaused else { return Observable<[CLLocation]>.empty() }
+                if isPaused {
+                    return Observable<[CLLocation]>.empty()
+                } else {
+                    return LocationManager.shared.rx.didUpdateLocations
+                }
+            }
+            .subscribe(onNext: {
+                guard let currentLocation = $0.last,
+                      let coordinate = lastCoordinate.value else { return }
+                distance.accept(distance.value + Int(currentLocation.distance(from: coordinate)))
+                lastCoordinate.accept(currentLocation)
+            })
+            .disposed(by: bag)
+        
+        return Output(startPosition: startPosition, currentPosition: currentPosition, isRecordStarted: recordStartTrigger.asDriver(), isRecordPaused: isRecordPaused.asDriver(), runningTime: runningTime.asDriver(), runningDistance: distance.asDriver())
+    }
+    
+    enum ButtonTapState {
+        case normal
+        case abnormal
     }
 }
