bedc9d55517ccdb734183eb1a4774ae596b757d0
Parkjju <rudwns3927@gmail.com>
2023-10-17 15:38:42 +0900

    [Feat] 경로 등록 뷰모델 정의 - 태그 셀렉팅 인풋 / 컬렉션뷰 아웃풋 릴레이객체 정의

--- runnerpia/Modules/RouteRegister/RouteRegisterViewModel.swift
+++ runnerpia/Modules/RouteRegister/RouteRegisterViewModel.swift
@@ -6,18 +6,35 @@
 //
 
 import Foundation
+import RxCocoa
+import RxSwift
 
 class RouteRegisterViewModel: ViewModelType {
     
     struct Input {
-        
+        let secureTagSelected: Observable<SecureTagCell>
+        let recommendedTagSelected: Observable<RecommendedTagCell>
     }
     
     struct Output {
-        
+        let secureTagCellItems: Driver<[SecureTagCellViewModel]>
+        let recommendedTagCellItems: Driver<[RecommendedTagCellViewModel]>
     }
     
     func transform(input: Input) -> Output {
-        return Output()
+        let secureTagItems = BehaviorRelay<[SecureTagCellViewModel]>(value: [])
+        let recommendedTagItems = BehaviorRelay<[RecommendedTagCellViewModel]>(value: [])
+        
+        secureTagItems.accept(SecureTags.retrieveAllSecureTagsWithArray()
+            .map {
+                SecureTagCellViewModel(with: $0)
+            })
+        
+        recommendedTagItems.accept(RecommendTags.retrieveAllRecommendedTagsWithArray()
+            .map {
+                RecommendedTagCellViewModel(with: $0)
+            })
+        
+        return Output(secureTagCellItems: secureTagItems.asDriver(), recommendedTagCellItems: recommendedTagItems.asDriver())
     }
 }
