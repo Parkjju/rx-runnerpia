ae8f60649a6a33d8c4eeb7e556852cb1fde35e0c
Parkjju <rudwns3927@gmail.com>
2023-10-17 15:27:58 +0900

    [Feat] 태그 rawValue 설정

--- runnerpia/Common/Tags/Tags.swift
+++ runnerpia/Common/Tags/Tags.swift
@@ -8,17 +8,25 @@
 import Foundation
 
 enum SecureTags: String {
-    case schoolZone
-    case safetyLight
-    case manyLight
-    case manyPeopleInNight
-    case manyPeopleInNoon
+    case schoolZone = "근처에 어린이 보호구역이 있어요"
+    case safetyLight = "안심등이 있어요"
+    case manyLight = "가로등이 많아요"
+    case manyPeopleInNight = "밤에 사람이 많아요"
+    case manyPeopleInNoon = "낮에 사람이 많아요"
+    
+    static func retrieveAllSecureTagsWithArray() -> [SecureTags] {
+        return [.schoolZone, .safetyLight, .manyLight, .manyPeopleInNoon, .manyPeopleInNight]
+    }
 }
 
 enum RecommendTags: String {
-    case riverView
-    case manyTrees
-    case cleanRoad
-    case noSteepRoad
-    case pedestrianOnly
+    case riverView = "강을 보며 달려요"
+    case manyTrees = "나무가 많아요"
+    case cleanRoad = "길이 깨끗해요"
+    case noSteepRoad = "가파른 구간이 없어요"
+    case pedestrianOnly = "보행자 전용 트랙이 있어요"
+    
+    static func retrieveAllRecommendedTagsWithArray() -> [RecommendTags] {
+        return [.riverView, .manyTrees, .cleanRoad, .noSteepRoad, .pedestrianOnly]
+    }
 }
