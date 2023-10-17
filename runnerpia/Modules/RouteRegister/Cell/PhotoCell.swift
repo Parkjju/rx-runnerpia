2994ff705aaac0dd493e3bddce937bc21054b474
Parkjju <rudwns3927@gmail.com>
2023-10-18 00:34:09 +0900

    [Feat] 이미지 컬렉션뷰 추가

---
+++ runnerpia/Modules/RouteRegister/Cell/PhotoCell.swift
@@ -0,0 +1,12 @@
+//
+//  PhotoCell.swift
+//  runnerpia
+//
+//  Created by Jun on 2023/10/18.
+//
+
+import UIKit
+
+class PhotoCell: UICollectionViewCell {
+    
+}
---
+++ runnerpia/Modules/RouteRegister/Cell/PhotoCellViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  PhotoCellViewModel.swift
+//  runnerpia
+//
+//  Created by Jun on 2023/10/18.
+//
+
+import Foundation
