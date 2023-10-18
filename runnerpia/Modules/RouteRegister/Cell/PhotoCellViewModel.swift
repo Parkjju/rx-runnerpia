7fcb8784fe4333cea6f374e5ae28e03a26497293
Parkjju <rudwns3927@gmail.com>
2023-10-18 10:25:03 +0900

    [Feat] 이미지 뷰모델 정의 / nil값기준 0번째 셀 분기처리

--- runnerpia/Modules/RouteRegister/Cell/PhotoCellViewModel.swift
+++ runnerpia/Modules/RouteRegister/Cell/PhotoCellViewModel.swift
@@ -6,3 +6,14 @@
 //
 
 import Foundation
+import RxCocoa
+import RxSwift
+
+class PhotoCellViewModel {
+    let photoCell = BehaviorRelay<UIImage?>(value: nil)
+    
+    init(with imageData: UIImage?) {
+        guard let imageData = imageData else { return }
+        photoCell.accept(imageData)
+    }
+}
